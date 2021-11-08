b12c0a9b06b4196d71634011060301608070aa40
Mike2217 <msoles2217@gmail.com>
2021-11-08 17:14:03 -0500

    third commit

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 
 
 
-getInput = input()
-
+// let getInput = document.querySelector('#dateInput1').value
+// console.log(getInput);
 async function getData() {
   try {
-    const res = await axios.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-10-17&end_date=2021-10-24&api_key=EbHvezIjiHvr628IlqtkTq3qC1BUKDla9pegqpkD')
+    const res = await axios.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-10-17&end_date=2021-10-20&api_key=EbHvezIjiHvr628IlqtkTq3qC1BUKDla9pegqpkD')
     console.log(res)
     console.log(res.data)
-    console.log(res.data.near_earth_objects.2021-10-17)
-    // const res1 = res.data.near_earth_objects
-    // console.log(res1)
+    console.log(res.data.near_earth_objects.forEach(() => {
+      
+    }))
   } 
   catch (err) {
     alert(err)
