19fac1091795def0ad90ba8275d5b8fc012d8f34
Mike2217 <msoles2217@gmail.com>
2021-11-12 15:36:58 -0500

    7th commit

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
 
+
+async function pageLoad() {
+  try {
+    const initData = await axios.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-10-17&end_date=2021-10-24&api_key=EbHvezIjiHvr628IlqtkTq3qC1BUKDla9pegqpkD')
+    // console.log(initData)
+  }
+  catch (err) {
+    alert(err)
+  }
+}
+pageLoad();
+
+
+
 document.querySelector('#firstButton').addEventListener('click', () => {
   document.querySelector('#fastestObject').classList.add('buttonChange')
   document.querySelector('#largestObject').classList.remove('buttonChange')
@@ -15,6 +29,39 @@ document.querySelector('#thirdButton').addEventListener('click', () => {
   document.querySelector('#fastestObject').classList.remove('buttonChange')
 })
 
+let fastButton = document.getElementById('fastestObject')
+let randomButton = document.getElementById('randomObject')
+let largeButton = document.getElementById('largestObject')
+
+
+
+const randomGallery = ['https://solarsystem.nasa.gov/system/resources/detail_files/1807_PIA15506_hires.jpg',
+  'https://solarsystem.nasa.gov/system/resources/detail_files/782_PIA02487.jpg',
+  'https://solarsystem.nasa.gov/system/resources/detail_files/511_pia18823.jpg',
+  'https://solarsystem.nasa.gov/system/resources/detail_files/788_PIA18899_modest.jpg',
+  'https://solarsystem.nasa.gov/system/resources/detail_files/2241_full_%281032x1032%29.gif',
+  'https://solarsystem.nasa.gov/system/content_pages/main_images/411_10199_chariklo_main.jpg']
+  
+console.log(randomGallery)
+
+// randomGalleryArray = [Object.values(randomGallery)]
+// console.log(randomGalleryArray)
+
+let fastestPictureURL = 'https://solarsystem.nasa.gov/system/resources/detail_files/518_CometISON1200w.jpg'
+let largestPictureURL = 'https://solarsystem.nasa.gov/system/content_pages/main_images/416_16_psyche_main.jpg'
+
+// console.log(randomPictureURL)
+
+
+
+
+
+
+
+
+
+
+
 
 
 let getSubmit = document.querySelector('#dataForm')
@@ -22,23 +69,22 @@ let getInput = document.querySelector('#dateInput1')
 
 getSubmit.addEventListener('submit', (e) => {
   e.preventDefault();
+
   console.log('submit complete')
   let dateEntry = getInput.value
   console.log(dateEntry)
-
   async function getData() {
     try {
       // const resDef = await axios.get('https://api.nasa.gov/neo/rest/v1/feed?start_date=2021-10-17&end_date=2021-10-17&api_key=EbHvezIjiHvr628IlqtkTq3qC1BUKDla9pegqpkD')
       const res = await axios.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${dateEntry}&end_date=${dateEntry}&api_key=EbHvezIjiHvr628IlqtkTq3qC1BUKDla9pegqpkD`)
       // console.log(resDef)
-      console.log(res)
-      console.log(res.data.near_earth_objects)
+      // console.log(res)
+      // console.log(res.data.near_earth_objects)
       let data = res.data.near_earth_objects[`${dateEntry}`]
-      // let data = res.data.near_earth_objects['2021-10-17']
       console.log(data)
+      // let dataArray = [data]
       let randomObject = data[Math.floor(Math.random() * data.length)]
       console.log(Object.keys(randomObject), Object.values(randomObject));
-      let randomObjectName = (randomObject.name)
       let randomObjectDiameter = Object.values(randomObject.estimated_diameter.meters)
       let randomObjectApproachDate = (randomObject.close_approach_data[0].close_approach_date)
       let randomObjectMissDistance = (randomObject.close_approach_data[0].miss_distance.miles)
@@ -46,16 +92,42 @@ getSubmit.addEventListener('submit', (e) => {
       // console.log(randomObjectApproachDate)
       // console.log(randomObjectRelativeVelocity)
       // console.log(randomObjectMissDistance)
-      let randomObjectData = (`Object Diameter: ${randomObjectDiameter} \n  Objects Relative Velocity: ${randomObjectSpeed}
-      Object Miss Distance: ${randomObjectMissDistance} \n Closest Approach Date: ${randomObjectApproachDate}`)
-      console.log(randomObjectData)
+      let randomObjectName = (`Near Earth Object: ${randomObject.name}`)
+      let randomObjectData = (`Object Diameter: ${randomObjectDiameter} \n  Objects Relative Velocity: ${randomObjectSpeed}miles per hour \n
+      Object Miss Distance: ${randomObjectMissDistance} miles \n Closest Approach Date: ${randomObjectApproachDate}`)
+      // console.log(randomObjectData)
       // document.getElementById('objectDataField').innerText = randomObjectData
-      document.getElementById('objectDataFieldTitle').innerText = randomObjectName
-      document.getElementById('objectDataField').innerText = randomObjectData
+      let objectPicture = document.querySelector('.objectPicture')
+      objectPicture.innerHTML = ''
+      if (randomButton.classList.contains('buttonChange')) {
+        document.getElementById('objectDataFieldTitle').innerText = randomObjectName
+        document.getElementById('objectDataField').innerText = randomObjectData
+        let randomPicture = document.createElement('img')
+        let randomPictureURL = randomGallery[Math.round(Math.random() * randomGallery.length)];
+        console.log(randomPictureURL)
+        console.log(randomPictureURL.length)
+        randomPicture.src = randomPictureURL
+        console.log(randomPicture)
+        objectPicture.appendChild(randomPicture)
+      }
+      else {
+        alert('You need to select an object')
+      }
+
+
+
+
+
+
+      // function dataFunction ()
+      // dataArray.forEach(() => {
+      //   console.log(data)
+      // })
     } catch (err) {
       alert(err)
     }
   }
-  getData()
+  getData();
 })
+
 
